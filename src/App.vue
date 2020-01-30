b1c0431e29b5eb489611a8ab7a600e5b12c15cf2
Sleque <info@sleque.com>
2020-01-30 11:22:54 -0500

    debugging

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,58 @@
 <template>
   <div id="app">
-    <Todos />
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todosObjects="todosObjects" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
-      Todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 1,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 1,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todosObjects: []
     }
+  },
+  methods: {
+    deleteTodo(id) {
+      //axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      //.then(res => this.todosObjects = this.todosObjects.filter(todosObjects => todosObjects.id !== id))
+      //.catch(err => console.log(err));
+
+      this.todosObjects = this.todosObjects.filter(todosObjects => todosObjects.id !== id);
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      
+      axios.post ('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todosObjects = [...this.todosObjects, res.data])
+        //.catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todosObjects = res.data)
+    //.catch(err => console.log(err));
   }
 }
 </script>
 
 <style>
-    {
+  *  {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
@@ -46,5 +61,18 @@ export default {
 body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <h1>Todos</h1>
+        <div v-bind:key="todosObjects.id" v-for="todosObjects in todosObjects">
+           <TodoItem v-bind:todosObjects="todosObjects" v-on:del-todo="$emit('del-todo', todosObjects.id)"/>
+        </div>
     </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue';
+
 export default {
-    name: "Todos"
-    
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todosObjects"]
 }
 </script>
 
