4d461a897b5e41c4f4a49b97c5f4676d69690d9c
Sleque <info@sleque.com>
2020-01-29 23:59:12 -0500

    init set up

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      Todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 1,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 1,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+    {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
